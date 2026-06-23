c9ba4e9148f856d5638950f4f128d16931436d8a
gade-6900 <gadepatil438@gmail.com>
2026-06-23 17:50:38 +0530

    space

--- ArrayutilityAppllication/index.js
+++ ArrayutilityAppllication/index.js
@@ -34,20 +34,6 @@ function filterEven() {
 
 // REDUCE
 function findSum() {
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
     let numbers = getNumbers();
 
     let sum = numbers.reduce((total, num) => total + num, 0);
